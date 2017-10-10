bf1a274ebdbebac0f3554dfd69a3e46bc029ac08
sirsh <sirsh@outlook.com>
2017-10-10 23:06:10 +0100

    added sample file to source

--- setup.py
+++ setup.py
@@ -10,23 +10,17 @@ setup(
     license='MIT',
     url='https://github.com/sirsh/sparkge',
     keywords='spark grammatical evolution',
-    description='using spark with the grammatical evolution method
-
-',
-    long_description=('using spark with the grammatical evolution method
-
-'),
+    description='using spark with the grammatical evolution method',
+    long_description=('using spark with the grammatical evolution method'),
     packages=['sparkge'],
     test_suite='nose.collector',
     tests_require=['nose'],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
+        'Intended Audience :: Developers,Researchers',
         'License :: MIT',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Communications :: Chat',
-        'Topic :: Internet',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
